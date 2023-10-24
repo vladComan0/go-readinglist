d5da8d8783f5db9d3e796e4f9dba8f27ef7f38ef
Vlad Coman <vladcoman@outlook.com>
2023-10-24 11:27:33 +0300

    Added models and handlers for web app routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,14 +4,20 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	"github.com/vladComan0/readinglist/internal/models"
 )
 
 type application struct {
+	readinglist *models.ReadinglistModel
 }
 
 func main() {
 	addr := flag.String("addr", ":80", "HTTP network address")
-	app := &application{}
+	endpoint := flag.String("endpoint", "http://localhost:4000/v1/books", "Endpoint for the reading list web service")
+	app := &application{
+		readinglist: &models.ReadinglistModel{Endpoint: *endpoint},
+	}
 	srv := &http.Server{
 		Addr:    *addr,
 		Handler: app.routes(),
